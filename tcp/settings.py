a3bf454c9a4ae31337ac0a5005d8cbf8e4ed0345
Austen Daniel Anop <austen626@outlook.com>
2021-04-29 04:17:37 -0500

    customer-service

--- tcp/settings.py
+++ tcp/settings.py
@@ -180,8 +180,8 @@ DEFAULT_EMAIL_BCC = ''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'developer@dcg.dev' #newaccounts@traviscapitalpartners.com'
-EMAIL_HOST_PASSWORD = 'Password#105'#'TCPnewDCG2020'#
+EMAIL_HOST_USER = 'developer@dcg.dev' #newaccounts@traviscapitalpartners.com'#'TCPnewDCG2020'#
+EMAIL_HOST_PASSWORD = 'gE7!Paym'#'Password#105'
 EMAIL_USE_SSL = False
 INVITE_TOKEN_URL = 'https://tcp2-dev-web-5scjsehyma-uk.a.run.app/register/?invite_token='#"https://dev-app.traviscapitalpartners.com/register/?invite_token="
 INVITE_TOKEN_URL_ADMIN = 'http://127.0.0.1:8000/register_dealer/?invite_token='
